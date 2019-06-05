68df7383fa164aa40c9d28ff704fa4096aae9124
Zachary Martin <zachary@truex.com>
2019-06-05 15:35:21 -0700

    Introduce Crashlytics
    Crashlytics handles crash reporting
    for our debug and release builds,
    allowing us to review any crashes
    that occur during testing or
    development.

--- TruexGoogleReferenceApp/src/main/java/com/truex/googlereferenceapp/MainApplication.java
+++ TruexGoogleReferenceApp/src/main/java/com/truex/googlereferenceapp/MainApplication.java
@@ -1,11 +1,24 @@
 package com.truex.googlereferenceapp;
 
+import com.crashlytics.android.Crashlytics;
 import com.truex.googlereferenceapp.dagger.DaggerAppComponent;
 
 import dagger.android.AndroidInjector;
 import dagger.android.DaggerApplication;
+import io.fabric.sdk.android.Fabric;
 
 public class MainApplication extends DaggerApplication {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Fabric.with(
+                new Fabric.Builder(this)
+                        .kits( new Crashlytics())
+                        .appIdentifier(BuildConfig.APPLICATION_ID)
+                        .build()
+        );
+    }
 
     @Override
     protected AndroidInjector<? extends MainApplication> applicationInjector() {
